02961042833fcfdcdc3deb5f746f3a33c0871edc
Kevin Schaper <kevinschaper@gmail.com>
2022-02-23 20:05:26 -0800

    Changed cli default output directory to the current directory

--- kghub_downloader/main.py
+++ kghub_downloader/main.py
@@ -9,7 +9,7 @@ typer_app = typer.Typer()
 @typer_app.command()
 def main(
          yaml_file: Optional[str] = typer.Argument("download.yaml", help="List of files to download in YAML format"),
-         output_dir: Optional[str] = typer.Option("data", help="Path to output directory"),
+         output_dir: Optional[str] = typer.Option(".", help="Path to output directory"),
          ignore_cache: Optional[bool] = typer.Option(False, help="Ignoring already downloaded files and download again"),
          tag: Optional[List[str]] = typer.Option(None, help="Optional list of tags to limit downloading to")
          ):
